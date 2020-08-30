09ca621ff2167693a00815f9a1b59e78fa31b77c
leonardodalinky <493987054@qq.com>
2020-08-31 01:42:57 +0800

    修改公会详细部分。完善文字内容排版，改善图表移动端观看体验。 同时修改首页换页条实现，以及修改首页字体。

--- src/Appbar.js
+++ src/Appbar.js
@@ -13,6 +13,7 @@ import MenuIcon from "@material-ui/icons/Menu"
 import ArrowBackIcon from "@material-ui/icons/ArrowBack"
 import SearchIcon from "@material-ui/icons/Search"
 import HistoryIcon from "@material-ui/icons/History"
+import GitHubIcon from '@material-ui/icons/GitHub';
 import {serverNameList} from "./Constant";
 import {withRouter} from "react-router"
 
@@ -83,7 +84,8 @@ const useStyles = makeStyles((theme) => ({
 const searchMethodText = ['公会名', '排名', '会长ID'];
 
 function AppHeader(props) {
-    const classes = useStyles()
+    const classes = useStyles();
+
     const handleClick = (e) => {
     }
     const [searchKeyword, setKeyword] = useState("");
@@ -141,12 +143,15 @@ function AppHeader(props) {
                             </Tooltip>
                         </>
                     )}
-                    <Tooltip title={"仓库还没建"}>
-                        <IconButton edge="start" color="inherit" aria-label="GitHub" className={classes.githubIcon}>
-                            <svg className="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
-                                <path
-                                    d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2c-3.3.7-4-1.6-4-1.6-.6-1.4-1.4-1.8-1.4-1.8-1-.7.1-.7.1-.7 1.2 0 1.9 1.2 1.9 1.2 1 1.8 2.8 1.3 3.5 1 0-.8.4-1.3.7-1.6-2.7-.3-5.5-1.3-5.5-6 0-1.2.5-2.3 1.3-3.1-.2-.4-.6-1.6 0-3.2 0 0 1-.3 3.4 1.2a11.5 11.5 0 0 1 6 0c2.3-1.5 3.3-1.2 3.3-1.2.6 1.6.2 2.8 0 3.2.9.8 1.3 1.9 1.3 3.2 0 4.6-2.8 5.6-5.5 5.9.5.4.9 1 .9 2.2v3.3c0 .3.1.7.8.6A12 12 0 0 0 12 .3"/>
-                            </svg>
+                    <Tooltip title={"个人Github仓库"}>
+                        <IconButton 
+                        edge="start" 
+                        color="inherit" 
+                        aria-label="GitHub" 
+                        className={classes.githubIcon}
+                        onClick={() => {window.location.assign('https://github.com/guch8017/ClanBattleFrontEnd')}}
+                        >
+                            <GitHubIcon />
                         </IconButton>
                     </Tooltip>
                 </Toolbar>
